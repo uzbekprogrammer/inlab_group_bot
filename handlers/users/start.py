5e5737153f5a8cbe08f679f2f02b909f80c40503
[CompilationError] - Mahmudov Abdurahim <82255958+uzbekprogrammer@users.noreply.github.com>
2021-11-09 15:12:02 +0300

    Update start.py

--- handlers/users/start.py
+++ handlers/users/start.py
@@ -1,9 +1,11 @@
 from aiogram import types
 from aiogram.dispatcher.filters.builtin import CommandStart
 from filters import IsPrivate
+from keyboards.default.menuKeyboard import Bookshelf
 from loader import dp
 
 
 @dp.message_handler(IsPrivate(), CommandStart())
 async def bot_start(message: types.Message):
-    await message.answer(f"Salom, {message.from_user.full_name}!")
+    await message.answer(f"Assalomu aleykum  {message.from_user.full_name}!\n"
+                         f"InLAB academy rasmiy botiga xush kelibsiz.", reply_markup=Bookshelf)
