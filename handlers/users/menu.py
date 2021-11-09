7f5606b2f840a963ba14a6478505615b53131110
uzbekprogrammer <mahmudovabdurahim04@gmail.com>
2021-11-09 17:29:41 +0300

    update menu

--- handlers/users/menu.py
+++ handlers/users/menu.py
@@ -7,21 +7,21 @@ from keyboards.default.itKursKey import itKurs
 from loader import dp
 
 
-@dp.message_handler(IsPrivate(),text='Fan kurslarimiz')
+@dp.message_handler(IsPrivate(), text='Fan kurslarimiz')
 async def show_fan_kurslar(message: Message):
     await message.answer(text='Bizda quyidagi kurslar mavjud :',reply_markup=fanKursKey)
 
 
-@dp.message_handler(IsPrivate(),text='IT kurslar')
+@dp.message_handler(IsPrivate(), text='IT kurslar')
 async def show_it_kurs(message: Message):
     await message.answer(text="Bizda quyidagi IT kurslar mavjud: ", reply_markup=itKurs)
 
 
-@dp.message_handler(IsPrivate(),text='Biz bilan bog\'lanish')
+@dp.message_handler(IsPrivate(), text='Biz bilan bog\'lanish')
 async def show_we(message:Message):
-    await message.answer(text="<a href='http://t.me/Nuriddin_Sobirov'>Bizga</a> bog'laning")
+    await message.answer(text="<a href='@AbduqodirIELTS_9'>Abduqodir Toshpo'latov</a>ga bog'laning")
 
 
-@dp.message_handler(IsPrivate(),text='Orqaga 🔙')
+@dp.message_handler(IsPrivate(), text='Orqaga 🔙')
 async def asadjas(message:Message):
     await message.answer(text='Orqaga qaytildi',reply_markup=Bookshelf)
