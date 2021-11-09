db53e32e56d6dc72f994338af90561da69edce50
[CompilationError] - Mahmudov Abdurahim <82255958+uzbekprogrammer@users.noreply.github.com>
2021-11-09 17:04:08 +0500

    Add files via upload

---
+++ handlers/users/menu.py
@@ -0,0 +1,27 @@
+from aiogram.dispatcher.filters import Command
+from aiogram.types import Message
+
+from filters import IsPrivate
+from keyboards.default import fanKursKey, Bookshelf
+from keyboards.default.itKursKey import itKurs
+from loader import dp
+
+
+@dp.message_handler(IsPrivate(),text='Fan kurslarimiz')
+async def show_fan_kurslar(message: Message):
+    await message.answer(text='Bizda quyidagi kurslar mavjud :',reply_markup=fanKursKey)
+
+
+@dp.message_handler(IsPrivate(),text='IT kurslar')
+async def show_it_kurs(message: Message):
+    await message.answer(text="Bizda quyidagi IT kurslar mavjud: ", reply_markup=itKurs)
+
+
+@dp.message_handler(IsPrivate(),text='Biz bilan bog\'lanish')
+async def show_we(message:Message):
+    await message.answer(text="<a href='http://t.me/Nuriddin_Sobirov'>Bizga</a> bog'laning")
+
+
+@dp.message_handler(IsPrivate(),text='Orqaga 🔙')
+async def asadjas(message:Message):
+    await message.answer(text='Orqaga qaytildi',reply_markup=Bookshelf)
